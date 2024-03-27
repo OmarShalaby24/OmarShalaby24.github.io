cda7bbb99953af42c396c01b87e2b4c8c62abe4a
OmarShalaby24 <ramo_24@outlook.com>
2024-03-28 01:48:49 +0200

    touches

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ function NavBar() {
         class="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarText"
-        aria-controls="navbarText"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
@@ -23,8 +23,8 @@ function NavBar() {
       >
         <ul className="navbar-nav justify-content-end">
           <li className="nav-item">
-            <a className="nav-link " style={{ color: "#ff0" }} href="#">
-              Work
+            <a className="nav-link " style={{ color: "#eee" }} href="#">
+              Work<span class="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
